aeeecde166616ae52cb3151ea39902d99716d62d
Kr1bas <116639223+Kr1bas@users.noreply.github.com>
2023-07-15 13:31:27 +0200

    Delete get_pk.sh

--- get_pk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-for i in {011..151}
-do 
-    echo "Downloading $i";
-    wget "https://assets.pokemon.com/assets/cms2/img/pokedex/full/$i.png" -O "assets/images/$i.png"
-    sleep 5
-done
