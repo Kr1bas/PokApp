78e0498e49d399a2f56d7e0af2fbd3399bd39e8c
Kr1bas <mirco.p98+github@gmail.com>
2023-07-17 17:02:30 +0200

    Updated NumberGuesser

--- get_pk.sh
+++ get_pk.sh
@@ -1,7 +1,7 @@
-#!/bin/zsh
-for i in {011..151}
+#!/bin/bash
+for i in {758..1010}
 do 
     echo "Downloading $i";
-    wget "https://assets.pokemon.com/assets/cms2/img/pokedex/full/$i.png" -O "assets/images/$i.png"
-    sleep 5
+    wget "https://assets.pokemon.com/assets/cms2/img/pokedex/full/$i.png" -O "assets/images/dex/$i.png"
+    sleep 8
 done
--- lib/number_guesser.dart
+++ lib/number_guesser.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pokapp/parallax_scrolling.dart';
 import 'dart:math';
 
@@ -8,7 +12,7 @@ class NumberGuesserHomePage extends StatelessWidget {
   List<DisplayPageItem> getNumberGuesserPageList() {
     final widgets = <DisplayPageItem>[];
     widgets.add(DisplayPageItem(
-      navigateTo: NumberGuesserGamePage(min: 1, max: 151),
+      navigateTo: const NumberGuesserGamePage(min: 1, max: 151),
       title: "First Generation",
       subTitle: "Only the original 151!",
       assetImage: "assets/images/covers/pk_frlg.jpg",
@@ -32,18 +36,22 @@ class NumberGuesserHomePage extends StatelessWidget {
 }
 
 class NumberGuesserGamePage extends StatefulWidget {
-  NumberGuesserGamePage({super.key, required this.max, required this.min});
+  const NumberGuesserGamePage(
+      {super.key, required this.max, required this.min});
 
-  int max;
-  int min;
+  final int max;
+  final int min;
   @override
   State<NumberGuesserGamePage> createState() => _NumberGuesserGamePageState();
 }
 
 class _NumberGuesserGamePageState extends State<NumberGuesserGamePage> {
-  int currentScore = 0;
-  int currentPick = -1;
-
+  int _currentScore = 0;
+  int _currentPick = -1;
+  int _currentLife = -1;
+  final List<int> _alreadyExtracted = [];
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _inputTextController = TextEditingController();
   final Random _rng = Random();
 
   @override
@@ -51,40 +59,170 @@ class _NumberGuesserGamePageState extends State<NumberGuesserGamePage> {
     super.initState();
   }
 
-  void next() {
-    currentPick = _rng.nextInt(widget.max - 1) + widget.min;
+  void _start() {
+    _currentLife = 3;
+    _currentPick = _rng.nextInt(widget.max - 1) + widget.min;
+    _alreadyExtracted.add(_currentPick);
+  }
+
+  void _next() {
+    if (_formKey.currentState!.validate()) {
+      if (int.parse(_inputTextController.text) == _currentPick) {
+        _currentScore += 1;
+        do {
+          _currentPick = _rng.nextInt(widget.max - 1) + widget.min;
+        } while (_alreadyExtracted.contains(_currentPick));
+      } else {
+        _currentLife -= 1;
+      }
+      _formKey.currentState!.reset();
+    }
+  }
+
+  List<Widget> _getCurrentLifeIcons() {
+    List<Widget> icons = [];
+    for (var i = 0; i < _currentLife; i++) {
+      icons.add(const Icon(
+        Icons.favorite,
+        color: Colors.red,
+      ));
+    }
+    for (var i = 0; i < (3 - _currentLife); i++) {
+      icons.add(const Icon(
+        Icons.heart_broken,
+        color: Colors.black12,
+      ));
+    }
+    return icons;
   }
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> children;
-    if (currentPick == -1) {
-      children = <Widget>[
-        TextButton(
-            onPressed: (() => setState(() => next())),
-            child: const Text("START!"))
-      ];
+    Widget child;
+    if (_currentPick == -1) {
+      child = Padding(
+        padding: const EdgeInsets.fromLTRB(10, 100, 10, 100),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset.zero,
+                  blurRadius: 4,
+                  spreadRadius: 2,
+                  blurStyle: BlurStyle.normal)
+            ],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: ElevatedButton(
+            onPressed: (() => setState(() => _start())),
+            child: const Text("START!"),
+          ),
+        ),
+      );
+    } else if (_currentLife == 0) {
+      child = Padding(
+        padding: const EdgeInsets.fromLTRB(10, 100, 10, 100),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset.zero,
+                  blurRadius: 4,
+                  spreadRadius: 2,
+                  blurStyle: BlurStyle.normal)
+            ],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(children: [
+            ListTile(
+              title: const Text("You final score is:"),
+              trailing: Text("$_currentScore"),
+            ),
+            ElevatedButton(
+              onPressed: (() => setState(() => _start())),
+              child: const Text("RESTART!"),
+            )
+          ]),
+        ),
+      );
     } else {
-      children = [
-        Text("Current Score: $currentScore"),
-        Image.asset(
-          "assets/images/dex/${currentPick < 10 ? '00$currentPick' : (currentPick < 100 ? '0$currentPick' : '$currentPick')}.png",
-          scale: 1.5,
+      child = Padding(
+        padding: const EdgeInsets.fromLTRB(10, 100, 10, 100),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset.zero,
+                  blurRadius: 4,
+                  spreadRadius: 2,
+                  blurStyle: BlurStyle.normal)
+            ],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text("Current Score: $_currentScore"),
+            GridView.count(
+              crossAxisCount: 1,
+              scrollDirection: Axis.horizontal,
+              children: _getCurrentLifeIcons(),
+            ),
+            Image.asset(
+              "assets/images/dex/${_currentPick < 10 ? '00$_currentPick' : (_currentPick < 100 ? '0$_currentPick' : '$_currentPick')}.png",
+              fit: BoxFit.contain,
+            ),
+            Form(
+              key: _formKey,
+              child: SizedBox(
+                width: 250,
+                child: TextFormField(
+                  controller: _inputTextController,
+                  decoration:
+                      const InputDecoration(hintText: "Insert number here"),
+                  keyboardType: TextInputType.number,
+                  autocorrect: false,
+                  maxLength: 4,
+                  maxLengthEnforcement:
+                      MaxLengthEnforcement.truncateAfterCompositionEnds,
+                  validator: ((value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a value';
+                    }
+                    if (int.parse(value) < widget.min ||
+                        int.parse(value) > widget.max) {
+                      return 'Value must be between ${widget.min} and ${widget.max}.';
+                    }
+                    return null;
+                  }),
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () => setState(() => _next()),
+                child: const Text("Next!")),
+          ]),
         ),
-        TextButton(
-            onPressed: () => setState(() => next()), child: const Text("Next!"))
-      ];
+      );
     }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("PokAPP"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: children,
-        ),
-      ),
+    return Stack(
+      children: <Widget>[
+        Image.asset("assets/images/other/pk_wp_2.jpg",
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            fit: BoxFit.cover),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: const Text("PokAPP"),
+          ),
+          body: Center(child: child),
+        )
+      ],
     );
   }
 }
