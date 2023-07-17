0e40236427ebc72f4fd807daf4bc4731f56a8609
Kr1bas <mirco.p98+github@gmail.com>
2023-07-17 18:46:43 +0200

    Added constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:pokapp/main.dart';
+
+const int kantoStart = 1;
+const int kantoEnd = 151;
+const int jhotoStart = 152;
+const int jhotoEnd = 251;
+const int hoennStart = 252;
+const int hoennEnd = 386;
+const int sinnohStart = 387;
+const int sinnohEnd = 493;
+const int unimaStart = 494;
+const int unimaEnd = 649;
+const int kalosStart = 650;
+const int kalosEnd = 721;
